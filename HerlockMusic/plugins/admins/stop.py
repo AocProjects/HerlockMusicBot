45ed56e655b4eb8b7505264bf486bde1af8e0900
SakirBey1 <sakirhack81@gmail.com>
2022-07-30 18:00:34 +0300

    Update stop.py

--- HerlockMusic/plugins/admins/stop.py
+++ HerlockMusic/plugins/admins/stop.py
@@ -12,10 +12,10 @@ from pyrogram.types import Message
 
 from config import BANNED_USERS
 from strings import get_command
-from YukkiMusic import app
-from YukkiMusic.core.call import Yukki
-from YukkiMusic.utils.database import set_loop
-from YukkiMusic.utils.decorators import AdminRightsCheck
+from HerlockMusic import app
+from HerlockMusic.core.call import Herlock
+from HerlockMusic.utils.database import set_loop
+from HerlockMusic.utils.decorators import AdminRightsCheck
 
 # Commands
 STOP_COMMAND = get_command("STOP_COMMAND")
@@ -31,7 +31,7 @@ STOP_COMMAND = get_command("STOP_COMMAND")
 async def stop_music(cli, message: Message, _, chat_id):
     if not len(message.command) == 1:
         return await message.reply_text(_["general_2"])
-    await Yukki.stop_stream(chat_id)
+    await Herlock.stop_stream(chat_id)
     await set_loop(chat_id, 0)
     await message.reply_text(
         _["admin_9"].format(message.from_user.mention)
