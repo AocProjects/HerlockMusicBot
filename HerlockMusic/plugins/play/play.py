6217edf2839feaf70562ebccfbef35e5c6c3f940
SakirBey1 <sakirhack81@gmail.com>
2022-07-30 18:21:13 +0300

    Update play.py

--- HerlockMusic/plugins/play/play.py
+++ HerlockMusic/plugins/play/play.py
@@ -19,21 +19,21 @@ from pytgcalls.exceptions import NoActiveGroupCall
 import config
 from config import BANNED_USERS, lyrical
 from strings import get_command
-from YukkiMusic import (Apple, Resso, SoundCloud, Spotify, Telegram,
+from HerlockMusic import (Apple, Resso, SoundCloud, Spotify, Telegram,
                         YouTube, app)
-from YukkiMusic.core.call import Yukki
-from YukkiMusic.utils import seconds_to_min, time_to_seconds
-from YukkiMusic.utils.channelplay import get_channeplayCB
-from YukkiMusic.utils.database import is_video_allowed
-from YukkiMusic.utils.decorators.language import languageCB
-from YukkiMusic.utils.decorators.play import PlayWrapper
-from YukkiMusic.utils.formatters import formats
-from YukkiMusic.utils.inline.play import (livestream_markup,
+from HerlockMusic.core.call import Herlock
+from HerlockMusic.utils import seconds_to_min, time_to_seconds
+from HerlockMusic.utils.channelplay import get_channeplayCB
+from HerlockMusic.utils.database import is_video_allowed
+from HerlockMusic.utils.decorators.language import languageCB
+from HerlockMusic.utils.decorators.play import PlayWrapper
+from HerlockMusic.utils.formatters import formats
+from HerlockMusic.utils.inline.play import (livestream_markup,
                                           playlist_markup,
                                           slider_markup, track_markup)
-from YukkiMusic.utils.inline.playlist import botplaylist_markup
-from YukkiMusic.utils.logger import play_logs
-from YukkiMusic.utils.stream.stream import stream
+from HerlockMusic.utils.inline.playlist import botplaylist_markup
+from HerlockMusic.utils.logger import play_logs
+from HerlockMusic.utils.stream.stream import stream
 
 # Command
 PLAY_COMMAND = get_command("PLAY_COMMAND")
@@ -335,7 +335,7 @@ async def play_commnd(
             return await mystic.delete()
         else:
             try:
-                await Yukki.stream_call(url)
+                await Herlock.stream_call(url)
             except NoActiveGroupCall:
                 await mystic.edit_text(
                     "There's an issue with the bot. Please report it to my owner and ask them to check logger group."
@@ -599,7 +599,7 @@ async def anonymous_check(client, CallbackQuery):
 
 
 @app.on_callback_query(
-    filters.regex("YukkiPlaylists") & ~BANNED_USERS
+    filters.regex("HerlockPlaylists") & ~BANNED_USERS
 )
 @languageCB
 async def play_playlists_command(client, CallbackQuery, _):
