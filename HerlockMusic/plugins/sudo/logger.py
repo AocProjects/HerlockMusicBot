1c8cdbc75e741009ecf6ff1185a41c88d7f31b58
SakirBey1 <sakirhack81@gmail.com>
2022-07-30 20:19:02 +0300

    Update logger.py

--- HerlockMusic/plugins/sudo/logger.py
+++ HerlockMusic/plugins/sudo/logger.py
@@ -11,10 +11,10 @@ from pyrogram import filters
 
 import config
 from strings import get_command
-from YukkiMusic import app
-from YukkiMusic.misc import SUDOERS
-from YukkiMusic.utils.database import add_off, add_on
-from YukkiMusic.utils.decorators.language import language
+from HerlockMusic import app
+from HerlockMusic.misc import SUDOERS
+from HerlockMusic.utils.database import add_off, add_on
+from HerlockMusic.utils.decorators.language import language
 
 # Commands
 LOGGER_COMMAND = get_command("LOGGER_COMMAND")
