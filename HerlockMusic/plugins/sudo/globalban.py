2f7ba5a6eaf4cc41cdaa9c8a865d05b97b5a7faa
SakirBey1 <sakirhack81@gmail.com>
2022-07-30 20:14:40 +0300

    Update globalban.py

--- HerlockMusic/plugins/sudo/globalban.py
+++ HerlockMusic/plugins/sudo/globalban.py
@@ -15,16 +15,16 @@ from pyrogram.types import Message
 
 from config import BANNED_USERS
 from strings import get_command
-from YukkiMusic import app
-from YukkiMusic.misc import SUDOERS
-from YukkiMusic.utils import get_readable_time
-from YukkiMusic.utils.database import (add_banned_user,
+from HerlockMusic import app
+from HerlockMusic.misc import SUDOERS
+from HerlockMusic.utils import get_readable_time
+from HerlockMusic.utils.database import (add_banned_user,
                                        get_banned_count,
                                        get_banned_users,
                                        get_served_chats,
                                        is_banned_user,
                                        remove_banned_user)
-from YukkiMusic.utils.decorators.language import language
+from HerlockMusic.utils.decorators.language import language
 
 # Command
 GBAN_COMMAND = get_command("GBAN_COMMAND")
