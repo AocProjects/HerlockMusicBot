79e998fdd5cc59aeae3c9d65c72b9ff4b0b61d45
SakirBey1 <sakirhack81@gmail.com>
2022-07-30 18:19:04 +0300

    Update live.py

--- HerlockMusic/plugins/play/live.py
+++ HerlockMusic/plugins/play/live.py
@@ -10,10 +10,10 @@
 from pyrogram import filters
 
 from config import BANNED_USERS
-from YukkiMusic import YouTube, app
-from YukkiMusic.utils.channelplay import get_channeplayCB
-from YukkiMusic.utils.decorators.language import languageCB
-from YukkiMusic.utils.stream.stream import stream
+from HerlockMusic import YouTube, app
+from HerlockMusic.utils.channelplay import get_channeplayCB
+from HerlockMusic.utils.decorators.language import languageCB
+from HerlockMusic.utils.stream.stream import stream
 
 
 @app.on_callback_query(filters.regex("LiveStream") & ~BANNED_USERS)
