a54bc17c702adf714e9e9d5ba7382ae0d0757582
SakirBey1 <sakirhack81@gmail.com>
2022-07-30 18:16:55 +0300

    Update seeker.py

--- HerlockMusic/plugins/misc/seeker.py
+++ HerlockMusic/plugins/misc/seeker.py
@@ -12,11 +12,11 @@ import asyncio
 from pyrogram.types import InlineKeyboardMarkup
 
 from strings import get_string
-from YukkiMusic.misc import db
-from YukkiMusic.utils.database import (get_active_chats, get_lang,
+from HerlockMusic.misc import db
+from HerlockMusic.utils.database import (get_active_chats, get_lang,
                                        is_music_playing)
-from YukkiMusic.utils.formatters import seconds_to_min
-from YukkiMusic.utils.inline import (stream_markup_timer,
+from HerlockMusic.utils.formatters import seconds_to_min
+from HerlockMusic.utils.inline import (stream_markup_timer,
                                      telegram_markup_timer)
 
 from ..admins.callback import wrong
