e3808d13fa41842294db312bbc890c756fe2c735
SakirBey1 <sakirhack81@gmail.com>
2022-07-30 20:26:10 +0300

    Update videomode.py

--- HerlockMusic/plugins/sudo/videomode.py
+++ HerlockMusic/plugins/sudo/videomode.py
@@ -12,10 +12,10 @@ from pyrogram.types import Message
 
 import config
 from strings import get_command
-from YukkiMusic import app
-from YukkiMusic.misc import SUDOERS
-from YukkiMusic.utils.database import add_off, add_on
-from YukkiMusic.utils.decorators.language import language
+from HerlockMusic import app
+from HerlockMusic.misc import SUDOERS
+from HerlockMusic.utils.database import add_off, add_on
+from HerlockMusic.utils.decorators.language import language
 
 # Commands
 VIDEOMODE_COMMAND = get_command("VIDEOMODE_COMMAND")
