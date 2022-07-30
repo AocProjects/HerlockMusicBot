df52ab4130642d733281e3d71052d9c756f3b479
SakirBey1 <sakirhack81@gmail.com>
2022-07-30 15:05:42 +0300

    Update __main__.py

--- HerlockMusic/__main__.py
+++ HerlockMusic/__main__.py
@@ -16,10 +16,10 @@ from pytgcalls.exceptions import NoActiveGroupCall
 
 import config
 from config import BANNED_USERS
-from YukkiMusic import LOGGER, app, userbot
-from YukkiMusic.core.call import Yukki
-from YukkiMusic.plugins import ALL_MODULES
-from YukkiMusic.utils.database import get_banned_users, get_gbanned
+from HerlockMusic import LOGGER, app, userbot
+from HerlockMusic.core.call import Herlock
+from HerlockMusic.plugins import ALL_MODULES
+from HerlockMusic.utils.database import get_banned_users, get_gbanned
 
 loop = asyncio.get_event_loop()
 
@@ -32,7 +32,7 @@ async def init():
         and not config.STRING4
         and not config.STRING5
     ):
-        LOGGER("YukkiMusic").error(
+        LOGGER("HerlockMusic").error(
             "No Assistant Clients Vars Defined!.. Exiting Process."
         )
         return
@@ -40,7 +40,7 @@ async def init():
         not config.SPOTIFY_CLIENT_ID
         and not config.SPOTIFY_CLIENT_SECRET
     ):
-        LOGGER("YukkiMusic").warning(
+        LOGGER("HerlockMusic").warning(
             "No Spotify Vars defined. Your bot won't be able to play spotify queries."
         )
     try:
@@ -54,28 +54,28 @@ async def init():
         pass
     await app.start()
     for all_module in ALL_MODULES:
-        importlib.import_module("YukkiMusic.plugins" + all_module)
-    LOGGER("Yukkimusic.plugins").info(
+        importlib.import_module("HerlockMusic.plugins" + all_module)
+    LOGGER("Herlockmusic.plugins").info(
         "Successfully Imported Modules "
     )
     await userbot.start()
     await Yukki.start()
     try:
-        await Yukki.stream_call(
+        await Herlock.stream_call(
             "http://docs.evostream.com/sample_content/assets/sintel1m720p.mp4"
         )
     except NoActiveGroupCall:
-        LOGGER("YukkiMusic").error(
+        LOGGER("HerlockMusic").error(
             "[ERROR] - \n\nPlease turn on your Logger Group's Voice Call. Make sure you never close/end voice call in your log group"
         )
         sys.exit()
     except:
         pass
-    await Yukki.decorators()
-    LOGGER("YukkiMusic").info("Yukki Music Bot Started Successfully")
+    await Herlock.decorators()
+    LOGGER("HerlockMusic").info("Herlock Music Bot Started Successfully")
     await idle()
 
 
 if __name__ == "__main__":
     loop.run_until_complete(init())
-    LOGGER("YukkiMusic").info("Stopping Yukki Music Bot! GoodBye")
+    LOGGER("HerlockMusic").info("Stopping Herlock Music Bot! GoodBye")
