b096967332ff7096ce5f09b9546ef2aa75ec517e
SakirBey1 <sakirhack81@gmail.com>
2022-07-30 20:32:58 +0300

    Update speedtest.py

--- HerlockMusic/plugins/tools/speedtest.py
+++ HerlockMusic/plugins/tools/speedtest.py
@@ -11,8 +11,8 @@ import asyncio
 import speedtest
 from pyrogram import filters
 from strings import get_command
-from YukkiMusic import app
-from YukkiMusic.misc import SUDOERS
+from HerlockMusic import app
+from HerlockMusic.misc import SUDOERS
 
 # Commands
 SPEEDTEST_COMMAND = get_command("SPEEDTEST_COMMAND")
