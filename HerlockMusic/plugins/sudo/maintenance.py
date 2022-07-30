131e65d41f6205b3ad1074d196966e1791f7ef0a
SakirBey1 <sakirhack81@gmail.com>
2022-07-30 20:20:14 +0300

    Update maintenance.py

--- HerlockMusic/plugins/sudo/maintenance.py
+++ HerlockMusic/plugins/sudo/maintenance.py
@@ -11,12 +11,12 @@ from pyrogram import filters
 from pyrogram.types import Message
 
 from strings import get_command, get_string
-from YukkiMusic import app
-from YukkiMusic.misc import SUDOERS
-from YukkiMusic.utils.database import (get_lang, is_maintenance,
+from HerlockMusic import app
+from HerlockMusic.misc import SUDOERS
+from HerlockMusic.utils.database import (get_lang, is_maintenance,
                                        maintenance_off,
                                        maintenance_on)
-from YukkiMusic.utils.decorators.language import language
+from HerlockMusic.utils.decorators.language import language
 
 # Commands
 MAINTENANCE_COMMAND = get_command("MAINTENANCE_COMMAND")
