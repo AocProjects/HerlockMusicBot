074a72e9037e239955eb7c169fd6cdb97b4081ed
SakirBey1 <sakirhack81@gmail.com>
2022-07-30 20:27:51 +0300

    Update languages.py

--- HerlockMusic/plugins/tools/languages.py
+++ HerlockMusic/plugins/tools/languages.py
@@ -13,9 +13,9 @@ from pyrogram.types import InlineKeyboardButton, Message
 
 from config import BANNED_USERS
 from strings import get_command, get_string, languages_present
-from YukkiMusic import app
-from YukkiMusic.utils.database import get_lang, set_lang
-from YukkiMusic.utils.decorators import (ActualAdminCB, language,
+from HerlockMusic import app
+from HerlockMusic.utils.database import get_lang, set_lang
+from HerlockMusic.utils.decorators import (ActualAdminCB, language,
                                          languageCB)
 
 # Languages Available
