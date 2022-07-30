21c2c54214bb62e0e840c50021b2e31129866762
SakirBey1 <sakirhack81@gmail.com>
2022-07-30 18:01:19 +0300

    Update unmute.py

--- HerlockMusic/plugins/admins/unmute.py
+++ HerlockMusic/plugins/admins/unmute.py
@@ -12,10 +12,10 @@ from pyrogram.types import Message
 
 from config import BANNED_USERS
 from strings import get_command
-from YukkiMusic import app
-from YukkiMusic.core.call import Yukki
-from YukkiMusic.utils.database import is_muted, mute_off
-from YukkiMusic.utils.decorators import AdminRightsCheck
+from HerlockMusic import app
+from HerlockMusic.core.call import Herlock
+from HerlockMusic.utils.database import is_muted, mute_off
+from HerlockMusic.utils.decorators import AdminRightsCheck
 
 # Commands
 UNMUTE_COMMAND = get_command("UNMUTE_COMMAND")
@@ -34,7 +34,7 @@ async def unmute_admin(Client, message: Message, _, chat_id):
     if not await is_muted(chat_id):
         return await message.reply_text(_["admin_7"])
     await mute_off(chat_id)
-    await Yukki.unmute_stream(chat_id)
+    await Herlock.unmute_stream(chat_id)
     await message.reply_text(
         _["admin_8"].format(message.from_user.mention)
     )
