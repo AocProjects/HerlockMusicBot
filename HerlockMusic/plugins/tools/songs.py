a9ae8b0cab8da3fcbc33f6729426d50687bb93c2
SakirBey1 <sakirhack81@gmail.com>
2022-07-30 20:32:20 +0300

    Update songs.py

--- HerlockMusic/plugins/tools/songs.py
+++ HerlockMusic/plugins/tools/songs.py
@@ -20,10 +20,10 @@ from pyrogram.types import (InlineKeyboardButton,
 from config import (BANNED_USERS, SONG_DOWNLOAD_DURATION,
                     SONG_DOWNLOAD_DURATION_LIMIT)
 from strings import get_command
-from YukkiMusic import YouTube, app
-from YukkiMusic.utils.decorators.language import language, languageCB
-from YukkiMusic.utils.formatters import convert_bytes
-from YukkiMusic.utils.inline.song import song_markup
+from HerlockMusic import YouTube, app
+from HerlockMusic.utils.decorators.language import language, languageCB
+from HerlockMusic.utils.formatters import convert_bytes
+from HerlockMusic.utils.inline.song import song_markup
 
 # Command
 SONG_COMMAND = get_command("SONG_COMMAND")
