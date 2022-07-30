d02f31ec5b4fef1a114751df7cf349be6d339e1a
SakirBey1 <sakirhack81@gmail.com>
2022-07-30 17:53:13 +0300

    Update loop.py

--- HerlockMusic/plugins/admins/loop.py
+++ HerlockMusic/plugins/admins/loop.py
@@ -12,10 +12,10 @@ from pyrogram.types import Message
 
 from config import BANNED_USERS
 from strings import get_command
-from YukkiMusic import app
-from YukkiMusic.utils.database.memorydatabase import (get_loop,
+from HerlockMusic import app
+from HerlockMusic.utils.database.memorydatabase import (get_loop,
                                                       set_loop)
-from YukkiMusic.utils.decorators import AdminRightsCheck
+from HerlockMusic.utils.decorators import AdminRightsCheck
 
 # Commands
 LOOP_COMMAND = get_command("LOOP_COMMAND")
