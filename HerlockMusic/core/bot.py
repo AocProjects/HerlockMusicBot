3233368f10ffce83be9fec070d326a32a9b95160
SakirBey1 <sakirhack81@gmail.com>
2022-08-04 23:55:10 +0300

    Update bot.py

--- HerlockMusic/core/bot.py
+++ HerlockMusic/core/bot.py
@@ -18,9 +18,9 @@ from ..logging import LOGGER
 
 class HerlockBot(Client):
     def __init__(self):
-        LOGGER(__name__).info(f"Starting Bot")
+        LOGGER(__name__).info(f"Bot Başlatıldı")
         super().__init__(
-            "YukkiMusicBot",
+            "HerlockMusicBot",
             api_id=config.API_ID,
             api_hash=config.API_HASH,
             bot_token=config.BOT_TOKEN,
@@ -33,21 +33,21 @@ class HerlockBot(Client):
         self.id = get_me.id
         try:
             await self.send_message(
-                config.LOG_GROUP_ID, "Bot Started"
+                config.LOG_GROUP_ID, "Bot Başlatıldı"
             )
         except:
             LOGGER(__name__).error(
-                "Bot has failed to access the log Group. Make sure that you have added your bot to your log channel and promoted as admin!"
+                "Bot, günlük Grubuna erişemedi. Botunuzu günlük kanalınıza eklediğinizden ve yönetici olarak terfi ettirdiğinizden emin olun!"
             )
             sys.exit()
         a = await self.get_chat_member(config.LOG_GROUP_ID, self.id)
         if a.status != "administrator":
             LOGGER(__name__).error(
-                "Please promote Bot as Admin in Logger Group"
+                "Lütfen Bot'u Logger Group'ta Yönetici olarak tanıtın"
             )
             sys.exit()
         if get_me.last_name:
             self.name = get_me.first_name + " " + get_me.last_name
         else:
             self.name = get_me.first_name
-        LOGGER(__name__).info(f"MusicBot Started as {self.name}")
+        LOGGER(__name__).info(f"MusicBot olarak başladı {self.name}")
